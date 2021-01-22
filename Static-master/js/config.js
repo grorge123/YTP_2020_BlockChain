aac8fe0d1636293a7febe25248f34237dd481d1e
treeleaves30760 <treeleaves30760@gmail.com>
2021-01-22 15:00:59 +0800

    First upload

---
+++ Static-master/js/config.js
@@ -0,0 +1,6 @@
+
+contractAddress = "0x75aEC234a8A018BbEd86fC5b641148A61C38d6bF";
+contractABI = "/js/abi.json";
+
+infuraAPI = "31846ed5f60c42a2b438d2def2c34ab9";
+imgurClientID = "dab9b4037fe92c0";
